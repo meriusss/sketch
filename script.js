4d6da38c699de72657bb50f8b844ababbcae861c
Marius <mariusginghina1@gmail.com>
2023-06-11 20:48:29 +0300

    Rewrote entire script using functions and added functionality

--- script.js
+++ script.js
@@ -2,48 +2,69 @@ const sketchArea = document.querySelector('.sketchArea');
 const buttonContainer = document.querySelector('.buttonContainer')
 const startButton = document.querySelector('#startButton');
 let color = document.querySelector("#color");
+squareArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
-
-function generateSquares(){
-    let squares = 256;
-    for(let i = 0; i < squares; i++){
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener('mousedown', (e)=>{
-            e.preventDefault();
-            square.style.backgroundColor = color.value;
-            });
+function getSquareNumber(){
     
-        square.addEventListener('contextmenu', (e)=>{
-            e.preventDefault();
-            square.style.backgroundColor = "white";
-            });        
-        
-            sketchArea.appendChild(square);};
-}
-
-function addResetButton(){
-    const squares = document.querySelectorAll(".square")
-    const resetButton = document.createElement("button");
-    resetButton.textContent = "Reset";
-    resetButton.classList.add("button");
-    resetButton.addEventListener('click', (e)=> {
-        squares.forEach(function(square) {
-            square.style.backgroundColor = "white";
-        })
-})
-    buttonContainer.appendChild(resetButton);
+    let squares = document.querySelector("#squaresSelector").value
+    if(squareArray.includes(parseInt(squares))){
+        console.log(parseInt(squares));
+        return parseInt(squares);
+    }
+    else {
+        alert("The number of squares must be between 1 and 16!");
+        return 0;
+    }
+    
 };
 
+function updateGridTemplateColumns(number){
+    
+    sketchArea.style.gridTemplateColumns = "";
+    for(let i = 0; i < number; i++){
+        sketchArea.style.gridTemplateColumns += " auto";
+    }
+    console.log(sketchArea.style.gridTemplateColumns);
+};
+
+
+function generateDrawableSquares(number){
+    
+    console.log("ok");
+    removeAllChildNodes(sketchArea);
+    for(let i = 0; i < Math.pow(parseInt(number), 2); i++){
+        const square = document.createElement("div");
+        square.classList.add("square");
+        sketchArea.appendChild(square);
+        drawSquare(square);}
+};
+
+function drawSquare(square){
+    
+    square.addEventListener('mousedown', (e)=>{
+        e.preventDefault();
+        square.style.backgroundColor = color.value;
+        });
+
+    square.addEventListener('contextmenu', (e)=>{
+        e.preventDefault();
+        square.style.backgroundColor = "white";
+        });
+}
+
+function removeAllChildNodes(parent) {
+    
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
 
 
 startButton.addEventListener('click', (e)=>{
     
-    generateSquares();
-    addResetButton();
-    startButton.remove();
-
-    
+    squares = getSquareNumber();
+    updateGridTemplateColumns(squares);
+    generateDrawableSquares(squares);
 
 });
